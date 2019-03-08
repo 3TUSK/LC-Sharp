6ee8278319b6ef1559a19ab68706c03d494be24f
3TUSK <urey.s.knowledge@gmail.com>
2019-03-07 22:08:00 -0800

    A fundamental LC-3 FSM unit test, also functioning as a basic regression test for future.

--- LC-Sharp/Test.cs
+++ LC-Sharp/Test.cs
@@ -11,7 +11,7 @@ namespace LC_Sharp
             [Test]
             public void SimpleAssembleTest()
             {
-                var source = new string[]
+                var source = new[]
                 {
                     "ADD R0, R0, #10",
                     "NOT R1, R1",
@@ -26,7 +26,26 @@ namespace LC_Sharp
                 Assert.Equals(assembled[3], 0xF021);
                 Assert.Equals(assembled[4], 0xF025);
             }
-        }    
+        }
+        
+        [TestFixture]
+        public class StateMachineTest
+        {
+            [Test]
+            public void StateMachineTest1()
+            {
+                var fsm = new LC3();
+                Assert.Equals(fsm.control.pc, 0x3000);
+                fsm.Execute(0b0001_001_001_1_01111); // ADD R1, R1, #15
+                Assert.Equals(fsm.processing.registers[1], 15);
+                Assert.Equals(fsm.control.pc, 0x3001);
+                Assert.False(fsm.processing.N);
+                Assert.False(fsm.processing.Z);
+                Assert.True(fsm.processing.P);
+                fsm.Execute(0b0011_001_000111101); // ST R1, LABEL_AT_X3040
+                Assert.Equals(fsm.memory.Read(0x3040), 0x000F); // 0x000F == 15
+            }
+        }
     }
 
 }
